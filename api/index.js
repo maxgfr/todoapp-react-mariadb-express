54346b385201bab2bee9c5c3c99fc0eeb3b9d443
@maxgfr <maxgolfier@gmail.com>
2019-05-24 10:08:14 +0200

    push

---
+++ api/index.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const port = 8000
+const mariadb = require('mariadb');
+const pool = mariadb.createPool({host: 'mydb.com', user: 'myUser', connectionLimit: 5});
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+  pool.getConnection()
+    .then(conn => {
+      conn.query("SELECT * from articles")
+        .then((res) => {
+          console.log(res);
+          conn.end();
+          res.json({data: res, success: true});
+        })
+        .catch(err => {
+          console.log(err);
+          conn.end();
+          res.json({error: err, success: false});
+        })
+
+    }).catch(err => {
+      console.log(err);
+      res.json({error: err, success: false});
+    });
+
+})
+
+app.post('/', (req, res) => {
+  console.log(req.body);
+  var author = '';
+  var status = '';
+  var section = '';
+  var date = '';
+  var title = '';
+  pool.getConnection()
+      .then(conn => {
+        conn.query("INSERT INTO articles value (?, ?, ?, ?, ?)", [author, status, section, date, title])
+          .then((res) => {
+            console.log(res);
+            conn.end();
+            res.json({data: res, success: true});
+          })
+          .catch(err => {
+            console.log(err);
+            conn.end();
+            res.json({error: err, success: false});
+          })
+      }).catch(err => {
+        console.log(err);
+        res.json({error: err, success: false});
+      });
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
