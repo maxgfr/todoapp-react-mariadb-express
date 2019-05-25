e5623142ba6ae18ad19cd85588a603f8d51ac093
@maxgfr <maxgolfier@gmail.com>
2019-05-25 10:20:01 +0200

    push

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+function Index() {
+  return <h2>Home</h2>;
+}
+
+function Articles() {
+  return <h2>About</h2>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/articles/">Add an rticles</Link>
+            </li>
+          </ul>
+        </nav>
+        <Route path="/" exact component={Index} />
+        <Route path="/articles/" component={Articles} />
+      </div>
+    </Router>
   );
 }
 
