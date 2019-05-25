fbecfae116905c0730059153a85f4eb15b0b99fb
@maxgfr <maxgolfier@gmail.com>
2019-05-25 11:24:06 +0200

    push

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # react-mariadb
 
-## Building & Running
+Application which connect a MariaDB database with a client-server application based on React.js and Express.js
+
+## Commands
 
 ```bash
+# to build and run it :
 docker-compose up --build
 # to stop and delete it :
 docker-compose down --rmi 'all'
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,4 +1,24 @@
 import React from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+
 
 class Home extends React.Component {
 
@@ -7,9 +27,45 @@ class Home extends React.Component {
   }
 
   render() {
-    return <h1>Hello</h1>;
+    return (
+        <Paper className={classes.root}>
+          <Table className={classes.table}>
+            <TableHead>
+              <TableRow>
+                <TableCell>Title</TableCell>
+                <TableCell align="right">Author</TableCell>
+                <TableCell align="right">Status</TableCell>
+                <TableCell align="right">Section</TableCell>
+                <TableCell align="right">Date</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map(row => (
+                <TableRow key={row.name}>
+                  <TableCell component="th" scope="row">{row.name}</TableCell>
+                  <TableCell align="right">{row.fat}</TableCell>
+                  <TableCell align="right">{row.carbs}</TableCell>
+                  <TableCell align="right">{row.carbs}</TableCell>
+                  <TableCell align="right">{row.protein}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </Paper>
+    );
   }
 
 }
+
+const classes = makeStyles(theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing(3),
+    overflowX: 'auto',
+  },
+  table: {
+    minWidth: 650,
+  },
+}));
 
 export default Home;
